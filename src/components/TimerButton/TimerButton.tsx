e654c2e2743e2901a3b8449e989b403aa854f8e2
Ahmed Khan <ahmed.khan8845@gmail.com>
2020-09-11 06:18:46 +0500

    Add Timer Components

---
+++ src/components/TimerButton/TimerButton.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './TimerButton.css';
+
+const TimerButton = (props: any) => (
+  <div className="button-container">
+    <button className="button-value">{props.buttonValue}</button>
+  </div>
+);
+
+TimerButton.propTypes = {
+  buttonAction: PropTypes.func.isRequired,
+  buttonValue: PropTypes.string.isRequired,
+};
+
+export default TimerButton;
